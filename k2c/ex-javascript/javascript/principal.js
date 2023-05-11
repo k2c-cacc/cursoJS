7734e1efba202f8e366967823b2e2c96748838e5
k2c-cacc <129217040+k2c-cacc@users.noreply.github.com>
2023-05-10 22:15:16 -0300

    ex-javascript

---
+++ k2c/ex-javascript/javascript/principal.js
@@ -0,0 +1,35 @@
+/*
+var i = 10
+console.log(i++ == 10)
+
+var i = 10
+console.log(++i == 10)
+*/
+//alert('Olá mundo! - principal.js')
+/*console.log(2**2)
+console.log(Math.pow(3, 3))
+console.log(Math.sqrt(49)) */
+/*
+var s1 = " Marcelo ";
+var s2 = " ";
+var s3 = " Martins ";
+
+// " Marcelo Martins "
+var s4 = s1 + s2 + s3;
+console.log(s4)
+
+var s1 = " Idade : ";
+ var idade = 30;
+
+ // " Idade : 30"
+ var s2 = s1 + idade
+console.log(s2) */
+/*
+console.log(1 + 2 + 3 + " testando ");
+console.log(" testando " + 1 + 2 + 3);
+*/
+var i = 10;
+
+var j = ++i + i--;
+
+console.log(j);
